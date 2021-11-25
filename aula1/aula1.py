a5330cef0b262559f3b230d2b82ef118df249a56
Luiz Paulo Araujo Ladeira <A238349@ae.local>
2021-11-25 14:44:26 -0300

    aulas 1,2 e 3

---
+++ aula1/aula1.py
@@ -0,0 +1,19 @@
+print('LINHA 01')
+print('LINHA 02')
+# FALANDO DO QUE SE TRATA ESSA LINHA
+print('LINHA 03')
+print('LINHA 04')  # ISSO É UM COMENTÁRIO
+print('LINHA 05')
+print('LINHA 06')
+
+# COMENTÁRIO MULTILINHA
+
+"""
+print('LINHA 01')
+print('LINHA 02')
+# FALANDO DO QUE SE TRATA ESSA LINHA
+print('LINHA 03')
+print('LINHA 04')  # ISSO É UM COMENTÁRIO
+print('LINHA 05')
+print('LINHA 06')
+"""
---
+++ aula2/aula2.py
@@ -0,0 +1,12 @@
+
+print(12345)
+print('LUIZ', 'LADEIRA')
+print('LUIZ', 'LADEIRA', sep="-", end="\n")
+print('LUIZ', 'GUSTAVO', sep="-", end="\n")
+
+"""
+824.176.070-18
+"""
+
+print('824','176','070', sep=".", end="-")
+print('18')
