ef21b8011f0f8cba953d96ca8acba170e0a70546
Luiz Paulo Araujo Ladeira <A238349@ae.local>
2022-01-13 11:19:55 -0300

    desafio de validador de cpf

--- desafioPratico_validaCPF/desafioValidarCpf.py
+++ desafioPratico_validaCPF/desafioValidarCpf.py
@@ -18,3 +18,33 @@ CPF: 168.995.350-09
 DIGITO 1 = 0               # DIGITO 2 = 9
 
 """
+
+while True:
+    cpf = input('Digite o seu CPF: ')
+    #cpf = '16899535009'
+    novo_cpf = cpf[:-2]  # retira os dois ultimos digitos
+    index_reverso = 10   # contador reverso
+    total = 0
+
+    for index in range(19):
+        if index > 8:   # indice vai de 0 a 9
+            index -= 9  # pegando os 9 primeiros digitos
+
+        print(cpf[index], index, index_reverso)
+        total += int(novo_cpf[index]) * index_reverso  # ele guarda a multiplicação na variavel total
+
+        index_reverso -= 1  # ele diminui o contador reverso
+        if index_reverso < 2:  # se ele for menor que dois ele começa a formula da segunda coluna
+            index_reverso = 11
+            digito = 11 - (total % 11)
+
+            if digito > 9:
+                digito = 0
+            total = 0
+            novo_cpf += str(digito)
+
+    print(novo_cpf)
+    if cpf == novo_cpf:
+        print(f'Seu CPF é Válido {cpf}')
+    else:
+        print(f'Seu CPF é inválido {cpf}')
